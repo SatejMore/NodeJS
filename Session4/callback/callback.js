3db7723c6fa20907d67ab0082eb8176b7dc8433e
Satej More <satejmore@gmail.com>
2018-05-30 22:53:41 +0530

    -Modified code to print the formatted body data as output.

--- Session4/callback/callback.js
+++ Session4/callback/callback.js
@@ -23,5 +23,5 @@ const request = require('request');
 request({
     url:"https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
     json:true},(error,response,body)=>{
-    console.log(body);
+    console.log(JSON.stringify(body,undefined,2));
 })
