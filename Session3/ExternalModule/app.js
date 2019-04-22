c26890f8683e6c96f1e8ab49202ceb6d9cfb4715
Satej More <satejmore@gmail.com>
2019-04-22 20:09:17 +0530

    -Fresh start.

--- Session3/ExternalModule/app.js
+++
@@ -1,6 +0,0 @@
-
-var _ = require('lodash');
-var array = ["aaa",1,1,2,2,3,4,4,"aaa"];
-
-var uniqueArray = _.uniq(array);
-console.log(uniqueArray);
--- Session3/SelfRequire/app.js
+++
@@ -1,5 +0,0 @@
-console.log("Starting app.js..")
-
-const selfModule = require('./self.js');
-
-console.log(`Addition is:${selfModule.add(10,20)}`);
